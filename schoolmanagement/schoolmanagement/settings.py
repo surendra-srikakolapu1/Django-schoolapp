c896bc7d36c1539cf3fe0134b32d738e208f529f
surendra-srikakolapu1 <85384978+surendra-srikakolapu1@users.noreply.github.com>
2021-09-28 14:08:46 +0530

    Update settings.py

--- schoolmanagement/schoolmanagement/settings.py
+++ schoolmanagement/schoolmanagement/settings.py
@@ -135,9 +135,3 @@ os.path.join(BASE_DIR,'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'schoolapp.surendra@gmail.com'
-EMAIL_HOST_PASSWORD = 'Passwordsn@123'
